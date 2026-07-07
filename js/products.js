aee4ff4cc844481f44ace3de04b1fb32129be5f0
drsamonline <sohilmomin2000@gmail.com>
2026-07-07 20:44:40 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,28 +1,34 @@
 fetch("../data/products.json")
-.then(response => response.json())
-.then(products => {
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("HTTP " + response.status);
+    }
+    return response.json();
+  })
+  .then(products => {
 
     const container = document.getElementById("product-list");
 
     products.forEach(product => {
 
-        container.innerHTML += `
-            <div class="category-card">
-                <h3>${product.title}</h3>
-
-                <p>${product.category}</p>
-
-                <strong>${product.price}</strong>
-
-                <br><br>
-
-                <a class="btn" href="${product.link}">
-                    View Product
-                </a>
-
-            </div>
-        `;
+      container.innerHTML += `
+        <div class="category-card">
+          <h3>${product.title}</h3>
+          <p>${product.category}</p>
+          <strong>${product.price}</strong>
+          <br><br>
+          <a class="btn" href="${product.link}">
+            View Product
+          </a>
+        </div>
+      `;
 
     });
 
-});
+  })
+  .catch(error => {
+    console.error(error);
+
+    document.getElementById("product-list").innerHTML =
+      "<p><strong>Error:</strong> " + error.message + "</p>";
+  });
